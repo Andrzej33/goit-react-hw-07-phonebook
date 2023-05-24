d7f91d18ae611b81f734d845bfc13147af387c81
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-05-24 16:57:37 +0300

    add selectFilteredContacts

--- src/Redux/selectors.jsx
+++ src/Redux/selectors.jsx
@@ -1,23 +1,15 @@
-// import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
 
-
-
-// export const selectFilteredContacts = createSelector(
-//     [selectContacts, selectFilter],
-//     (contacts, filterValue) => {
-//       console.log("Calculating visible tasks. Now memoized!");
-//       switch (statusFilter) {
-//         case statusFilters.active:
-//           return tasks.filter(task => !task.completed);
-//         case statusFilters.completed:
-//           return tasks.filter(task => task.completed);
-//         default:
-//           return tasks;
-//       }
-//     }
-//   );
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  ({ items }, filterValue) => {
+    return items.filter(item =>
+      item.name.toLowerCase().includes(filterValue.toLowerCase())
+    );
+  }
+);
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,40 +2,36 @@ import { ContactItem } from '../ContactItem/ContactItem';
 import { useEffect } from 'react';
 import { ListOfContacts } from './ContactList.styled';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts, selectFilter } from 'Redux/selectors';
+import { selectContacts, selectFilteredContacts } from 'Redux/selectors';
 import { fetchContacts } from 'API/API.Axios';
 import { ThreeDots } from 'react-loader-spinner';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const { items, isLoading, error } = useSelector(selectContacts);
+  const { isLoading, error } = useSelector(selectContacts);
+
+  const filteredContacts = useSelector(selectFilteredContacts);
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
-
-  const filterValue = useSelector(selectFilter);
-
-  const filteredContacts = items.filter(item =>
-    item.name.toLowerCase().includes(filterValue.toLowerCase())
-  );
-
-  if (filteredContacts.length === 0) {
-    return (
-      <h3 style={{ paddingLeft: '40px' }}>There are no apropriate contacts</h3>
-    );
-  }
 
   return (
     <ListOfContacts>
       {isLoading && <ThreeDots color="darkblue" />}
       {error && <b>{error}</b>}
-      {filteredContacts.map(contact => {
-        return (
-          <li key={contact.id}>
-            <ContactItem contact={contact} />
-          </li>
-        );
-      })}
+      {filteredContacts.length !== 0 ? (
+        filteredContacts.map(contact => {
+          return (
+            <li key={contact.id}>
+              <ContactItem contact={contact} />
+            </li>
+          );
+        })
+      ) : (
+        <h3 style={{ paddingLeft: '40px' }}>
+          There are no apropriate contacts
+        </h3>
+      )}
     </ListOfContacts>
   );
 };
